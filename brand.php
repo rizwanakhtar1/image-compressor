89689b310634336a3be5172c58159950c51c2a21
rizwanakhtar1 <78685422+rizwanakhtar1@users.noreply.github.com>
2023-04-08 01:02:22 +0530

    Add files via upload

---
+++ brand.php
@@ -0,0 +1,8 @@
+<?php
+	// $domainName = "CompressJPGOnline.com";
+	// $domainURL = "http://compressjpgonline.com";
+	$icon_loc = "./img/icon.png";
+	$main_title = "Compress JPG / JPEG Images Online for Free";  // Title of the Site
+	
+	$contact_email = "rizwan.akhtar.2121@gmail.com";
+?>
